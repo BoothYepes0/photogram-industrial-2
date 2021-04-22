49abbd1e8849e51b4972763edbc13b7bec0b21da
yepes0 <yepes0@chicagobooth.edu>
2021-04-22 03:49:43 +0000

    calling it a day.

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   resources :follow_requests
   resources :comments
   resources :photos
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

4f87a35912598da84a9c35e7068b9a272a43ad08
yepes0 <yepes0@chicagobooth.edu>
2021-04-23 22:11:59 +0000

    before starting part 5

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root "photos#index"
 
+
+  
+
   devise_for :users
 
   resources :likes
@@ -8,7 +11,7 @@ Rails.application.routes.draw do
   resources :comments
   resources :photos
   resources :users, only: :show
-
+  get "/:username" => "users#show" #most general
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
